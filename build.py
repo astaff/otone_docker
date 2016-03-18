4dcf158357a0fc940f210373199d8815f09429c1
astaff <artyom.astafurov@gmail.com>
2016-03-18 14:45:12 -0400

    Merged upstream, updated .gitignore to include dist, reverted build to two-stage process using -build suffix and dist to produce the distrubution, updated Dockerfile entrypoints accordingly

--- build.py
+++ build.py
@@ -22,8 +22,8 @@ containers = ["crossbar", "frontend", "driver", "labware"]
 
 for container in containers:
     subprocess.call(
-        "docker build -t {0} {0} && docker tag -f {0} {0}-$ARCH".format(container),
-        #&& docker run {0}-build > {0}/dist/root.tar.gz && docker build -t {0}-$ARCH {0}/dist".format(container),
+        """docker build -t {0}-build {0}
+         && docker run {0}-build > {0}/dist/root.tar.gz && docker build -t {0}-$ARCH {0}/dist""".format(container),
         shell=True)
     subprocess.call(
         "docker save {0}-$ARCH | docker-squash -t {1}/{0}-$ARCH:latest | docker load && docker push {1}/{0}-$ARCH:latest".format(container, repo),
