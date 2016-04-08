26868157f80e9fd5465955b1944be7eba242a9c5
Artyom Astafurov <artyom.astafurov@gmail.com>
2016-04-08 14:22:48 -0400

    Added docker tagging with a shorter name

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ sudo ln -fs $(pwd)/common-$ARCH $(pwd)/common
 for name in common common-build crossbar bootstrap
 do
   docker build -t $REPO/$name-$ARCH $name/. \
+  && docker tag $REPO/$name-$ARCH $name \
   && docker save $REPO/$name-$ARCH | gzip | aws s3 cp - s3://$AWS_BUCKET/builds/$name-$ARCH/$TRAVIS_BUILD_NUMBER/$REPO-$name-$ARCH.tar.gz \
   && aws s3 rm s3://$AWS_BUCKET/builds/$name-$ARCH/latest --recursive \
   && aws s3 cp s3://$AWS_BUCKET/builds/$name-$ARCH/$TRAVIS_BUILD_NUMBER s3://$AWS_BUCKET/builds/$name-$ARCH/latest/ --recursive
