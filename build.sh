79a6191d8158b2e3e71f2054d49ff0bf7331f8cf
Artyom Astafurov <artyom.astafurov@gmail.com>
2016-04-08 18:17:12 -0400

    Fixing build for arm

--- build.sh
+++ build.sh
@@ -7,14 +7,6 @@ pip install awscli
 # docker files
 sudo ln -fs $(pwd)/common-$ARCH $(pwd)/common
 
-# this trick is done to allow compiling ARM images ander x86
-# here we map arm executeable header to /usr/bin/qemu-arm-static
-# handler that is located in common-arm container
-# by doing this, we'll have arm environment in our container
-# produced from common-arm
-sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc  
-sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > /proc/sys/fs/binfmt_misc/register  
-
 # prepare all common containers
 for name in common common-build crossbar bootstrap
 do
